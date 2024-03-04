c06c14a461d025709288ad69e0d63d6def9cf4fd
Joe Salem <joesalem@bellsouth.net>
2024-03-03 21:22:20 -0500

    Completed unsprinkle exercise

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,42 +1,62 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
 const Hero = () => {
-  return (
-    <Wrapper>
-      <HeroImage src="/images/hero-img.jpg" />
-      <Swoop src="/swoop.svg" />
-    </Wrapper>
-  );
+   return (
+      <Wrapper>
+         <picture>
+            <source
+               type="image/avif"
+               srcset="
+                  /images/hero-img.avif 1x,
+                  /images/hero-img@2x.avif 2x,
+                  /images/hero-img@3x.avif 3x
+               "
+            />
+            <source
+               type="image/jpeg"
+               srcset="
+                  /images/hero-img.jpg 1x,
+                  /images/hero-img@2x.jpg 2x,
+                  /images/hero-img@3x.jpg 3x
+               "
+            />
+            <HeroImage alt="Sample site content: a cat staring back at the user." />
+         </picture>
+         <Swoop
+            alt="Decorative curved lower boundary to the staring cat."
+            src="/swoop.svg"
+         />
+      </Wrapper>
+   );
 };
 
 const Wrapper = styled.section`
-  position: relative;
-  height: 60vh;
-  min-height: 400px;
-  display: flex;
-  justify-content: center;
-  align-items: flex-end;
-  background: hsl(0deg 0% 1%);
+   position: relative;
+   height: 60vh;
+   min-height: 400px;
+   display: flex;
+   justify-content: center;
+   align-items: flex-end;
+   background: hsl(0deg 0% 1%);
 `;
 
 const HeroImage = styled.img`
-  display: block;
-  width: 500px;
-  height: 500px;
-  max-height: 100%;
+   display: block;
+   height: 500px;
+   max-height: 100%;
 `;
 
 const Swoop = styled.img`
-  position: absolute;
-  left: 0;
-  right: 0;
-  /*
+   position: absolute;
+   left: 0;
+   right: 0;
+   /*
     Overhang by a couple px to prevent any pixel-rounding
     display issues
   */
-  bottom: -2px;
-  width: 100%;
+   bottom: -2px;
+   width: 100%;
 `;
 
 export default Hero;
--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -1,47 +1,73 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
 const PhotoGridItem = ({ id, src, alt, tags }) => {
-  return (
-    <article>
-      <Anchor href={`/photos/${id}`}>
-        <Image src={src} />
-      </Anchor>
-      <Tags>
-        {tags.map((tag) => (
-          <Tag key={tag}>{tag}</Tag>
-        ))}
-      </Tags>
-    </article>
-  );
+   const srcPrefix = src.replace(".jpg", "");
+   const srcsetAvif = `
+    ${srcPrefix}.avif 1x, 
+    ${srcPrefix}@2x.avif 2x, 
+    ${srcPrefix}@3x.avif 3x
+  `;
+   const srcsetJpeg = `
+    ${srcPrefix}.jpg 1x, 
+    ${srcPrefix}@2x.jpg 2x, 
+    ${srcPrefix}@3x.jpg 3x
+  `;
+   return (
+      <article>
+         <Anchor href={`/photos/${id}`}>
+            <picture>
+               <source type="image/avif" srcSet={srcsetAvif} />
+               <source type="image/jpeg" srcSet={srcsetJpeg} />
+               <Image alt={alt} />
+            </picture>
+         </Anchor>
+         <Tags>
+            {tags.map((tag) => (
+               <Tag key={tag} title={tag}>
+                  {tag}
+               </Tag>
+            ))}
+         </Tags>
+      </article>
+   );
 };
 
 const Anchor = styled.a`
-  text-decoration: none;
-  color: inherit;
-  outline-offset: 4px;
+   text-decoration: none;
+   color: inherit;
+   outline-offset: 4px;
 `;
 
 const Image = styled.img`
-  display: block;
-  width: 100%;
-  height: 300px;
-  border-radius: 2px;
-  margin-bottom: 8px;
+   display: block;
+   border-radius: 2px;
+   margin-bottom: 8px;
+   width: 100%;
+   height: 300px;
+   object-fit: cover;
 `;
 
 const Tags = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 8px;
+   list-style-type: none;
+   white-space: nowrap;
+   overflow: hidden;
+   text-overflow: ellipsis;
+   padding: 4px 0;
 `;
 
 const Tag = styled.li`
-  padding: 4px 8px;
-  background: var(--color-gray-300);
-  font-size: 0.875rem;
-  font-weight: 475;
-  color: var(--color-gray-800);
+   display: inline;
+   padding: 4px 8px;
+   margin: 0 4px;
+   background: var(--color-gray-300);
+   font-size: 0.875rem;
+   font-weight: 475;
+   color: var(--color-gray-800);
+
+   &:first-of-type {
+      margin-left: 0;
+   }
 `;
 
 export default PhotoGridItem;
